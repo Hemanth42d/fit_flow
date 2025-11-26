c9d03efbfe2eedb3da81cf536058728635f0d259
Hemanth42d <venkatahemanth42d@gmail.com>
2025-11-26 19:30:31 +0530

    Changes done for uploading in github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Base path for GitHub Pages - change 'gymslot' to your repo name
+  // Base path for GitHub Pages - matches your repo name
   base: '/fit_flow/',
 })
